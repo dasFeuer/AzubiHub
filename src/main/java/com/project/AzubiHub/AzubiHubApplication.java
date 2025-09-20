74a897bcdc689e6f466ab60363de98c5f6227bd5
Barun <barunpanthisharma11@gmail.com>
2025-09-20 17:08:32 +0200

    First commit

---
+++ src/main/java/com/project/AzubiHub/AzubiHubApplication.java
@@ -0,0 +1,13 @@
+package com.project.AzubiHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzubiHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzubiHubApplication.class, args);
+	}
+
+}
