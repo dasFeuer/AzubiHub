abc013943d85d6ea41f9499ce45df747d3b54b95
Barun <barunpanthisharma11@gmail.com>
2025-09-21 15:20:51 +0200

    User entity added

---
+++ src/main/java/com/project/AzubiHub/Enum/Roles.java
@@ -0,0 +1,5 @@
+package com.project.AzubiHub.Enum;
+
+public enum Roles {
+    Azubi_User, Ausbilder_User, Admin_User
+}
---
+++ src/main/java/com/project/AzubiHub/enitty/User.java
@@ -0,0 +1,56 @@
+package com.project.AzubiHub.enitty;
+
+import com.project.AzubiHub.Enum.Roles;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Table(name = "users")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Roles roles;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(createdAt, user.createdAt) && Objects.equals(updatedAt, user.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, password, createdAt, updatedAt);
+    }
+}
